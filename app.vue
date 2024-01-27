88b377953802f0a22d5a80fb3f91fe59cd2607a4
Chris Bell <chrislbellny@gmail.com>
2024-01-26 18:12:05 -0600

    let's try connecting to supabase again

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://ijsufybnlehtsglhhytd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqc3VmeWJubGVodHNnbGhoeXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5NzY3ODcsImV4cCI6MjAyMTU1Mjc4N30.yOUt43TspCJCTBHPn21YVpYpU79k37KSPuh6Q-qAoy0')
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 </template>

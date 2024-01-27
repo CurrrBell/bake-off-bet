d68e6194aca360a11b44a6436666212130588489
Chris Bell <chrislbellny@gmail.com>
2024-01-26 20:15:02 -0600

    switch to the supabase nuxt plugin

--- app.vue
+++ app.vue
@@ -1,18 +1,19 @@
-<script setup>
-import { createClient } from '@supabase/supabase-js'
-// const supabase = createClient('https://ijsufybnlehtsglhhytd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqc3VmeWJubGVodHNnbGhoeXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5NzY3ODcsImV4cCI6MjAyMTU1Mjc4N30.yOUt43TspCJCTBHPn21YVpYpU79k37KSPuh6Q-qAoy0')
-const countries = ref([])
+<script setup lang="ts">
+const countries = ref([]);
 
-// async function getCountries() {
-//   const { data } = await supabase.from('countries').select()
-//   countries.value = data
-// }
+const client = useSupabaseClient();
 
-// onMounted(() => {
-//   getCountries()
-// })
+
+async function getCountries() {
+    const { data } = await client.from('countries').select();
+    countries.value = data ?? [];
+}
+
+onMounted(() => {
+  getCountries()
+});
+
 </script>
-
 <template>
   <ul v-if="countries.length > 0">
     <li v-for="country in countries" :key="country.id">{{ country.name }}</li>

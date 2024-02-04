08832a24c9baf6fe310c8bd2c04fd55980a6945b
Chris Bell <chrislbellny@gmail.com>
2024-02-04 16:36:18 -0600

    add nuxt ui

---
+++ layout/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/bets">Bets</NuxtLink>
+    </nav>
+    <main>
+        <slot />
+    </main>
+</template>
+<script setup lang="ts">
+</script>

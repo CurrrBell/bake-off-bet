06b54083ed8679685297b93c59c144a71752895d
Chris Bell <chrislbellny@gmail.com>
2024-02-05 18:32:13 -0600

    use nuxtUI's nav component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
-    <nav>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/bets">Bets</NuxtLink>
-    </nav>
+    <UHorizontalNavigation :links="links" />
     <main>
         <slot />
     </main>
 </template>
 <script setup lang="ts">
+const links = [
+    {
+        label: 'Home',
+        to: '/',
+    },
+    {
+        label: 'Bets',
+        to: '/bets',
+    },
+    {
+        label: 'My Bets',
+        to: '/bets/my-bets',
+    }
+]
 </script>
